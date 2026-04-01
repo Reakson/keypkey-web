23c8a74bed9218629e72b8700edc425f19e71e40
reakson <v.monireak15@gmail.com>
2026-04-01 23:53:00 +0700

    Change dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
-import { api } from '../utils/api';
+import { api } from '../api/client';
 
 function computeStrength(pwd = '') {
   let score = 0;
